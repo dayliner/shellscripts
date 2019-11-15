a1397a1a2a72a8b4f6cd01b60db0efb5e9e27c79
root user <root@domain.com>
2019-11-14 16:12:48 -0800

    First commit

---
+++ diskconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script creates a report of our disk configuration.
+
+FLUFFY_BUNNY=`hostname`
+
+echo "Disk report saved to $FLUFFY_BUNNY.report"
+
+echo -e "\n LVM Configuration:  \n\n"  >>$FLUFFY_BUNNY.report
+lvscan  >>$FLUFFY_BUNNY.report
+
+echo -e "\n\n Partition Configuration:  \n\n"   >>$FLUFFY_BUNNY.report
+fdisk -l | head -17  >>$FLUFFY_BUNNY.report
+
+echo -e "\n\n Mounted Filesystems: \n\n"  >>$FLUFFY_BUNNY.report
+df -hT | grep -v tmp >>$FLUFFY_BUNNY.report
